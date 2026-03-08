b00eeb58f91f865302099c0a2610ae9937fabd86
Harsh Srivastava <1by23ai058@bmsit.in>
2026-03-08 17:24:02 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 // Prisma 7 requires the Neon adapter to be passed directly to PrismaClient
